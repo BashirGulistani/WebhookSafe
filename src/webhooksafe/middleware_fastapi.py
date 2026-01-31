b0d99b470955095145edb371e4560e8869b095a2
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-30 22:13:00 -0500

    Create middleware_fastapi.py

---
+++ src/webhooksafe/middleware_fastapi.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from .sieve import WebhookSieve
+
+
+
+
+
+
+
+
